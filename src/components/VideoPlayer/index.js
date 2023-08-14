a1edf8bcadd63a65fee8e13e77a10365ada5e1ca
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-14 10:36:29 +0000

    session started

---
+++ src/components/VideoPlayer/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const VideoPlayer = () => (
+  <div className="video-container">
+    <h1 className="heading">Video Player</h1>
+    <div className="responsive-container">{/* Your code goes here */}</div>
+  </div>
+)
+
+export default VideoPlayer

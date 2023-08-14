fef0c78ba1cc300ffbf85e9acaee7758e6815f6a
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-14 15:01:18 +0000

    button added

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,28 +1,48 @@
+import {Component} from 'react'
+
 import ReactPlayer from 'react-player'
 
 import './index.css'
 
-const videoURL = 'https://youtu.be/7sDY4m8KNLc'
+// const videoURL = 'https://youtu.be/7sDY4m8KNLc'
+const videoURL = 'https://youtu.be/YE7VzlLtp-4'
 
-const VideoPlayer = () => (
-  <div className="video-container">
-    <h1 className="heading">Video Player</h1>
-    <div className="responsive-container">
-      <ReactPlayer
-        width="700px"
-        height="400px"
-        controls
-        pip
-        volume
-        url={videoURL}
-        onReady={() => console.log('onReady callback')}
-        onStart={() => console.log('onStart callback')}
-        onPause={() => console.log('onPause callback')}
-        onEnded={() => console.log('onEnded callback')}
-        onError={() => console.log('onError callback')}
-      />
-    </div>
-  </div>
-)
+class VideoPlayer extends Component {
+  state = {
+    isPlaying: false,
+  }
 
+  onClickPlay = () => {
+    this.setState(prevState => ({isPlaying: !prevState.isPlaying}))
+  }
+
+  render() {
+    const {isPlaying} = this.state
+    const btnText = isPlaying ? 'Pause' : 'Play'
+
+    return (
+      <div className="video-container">
+        <h1 className="heading">Video Player</h1>
+        <div className="responsive-container">
+          <ReactPlayer
+            playing={isPlaying}
+            // width="700px"
+            // height="400px"
+            controls
+            volume
+            url={videoURL}
+            onReady={() => console.log('onReady callback')}
+            onStart={() => console.log('onStart callback')}
+            onPause={() => console.log('onPause callback')}
+            onEnded={() => console.log('onEnded callback')}
+            onError={() => console.log('onError callback')}
+          />
+        </div>
+        <button type="button" className="button" onClick={this.onClickPlay}>
+          {btnText}
+        </button>
+      </div>
+    )
+  }
+}
 export default VideoPlayer
